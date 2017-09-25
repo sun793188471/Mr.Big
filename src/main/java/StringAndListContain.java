bc87c4901eb7607cc1941583017755564eae63a1
sunyukun <sunyukun@patsnap.com>
2017-09-25 14:01:38 +0800

    lizhi

---
+++ src/main/java/StringAndListContain.java
@@ -0,0 +1,10 @@
+/**
+ * Created by sunyukun on 2017/9/12.
+ */
+public class StringAndListContain {
+    public static void main(String[] args) {
+        String a = "sunyu";
+        String b = "sunyukun";
+        System.out.println(b.contains(a));
+    }
+}
